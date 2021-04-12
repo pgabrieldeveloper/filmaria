dfad4b24c7bcd113ba07f1b33739948d5ba85b20
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-12 13:48:33 -0300

    home page content

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
+import "./style.css";
 function Home() {
+  const [filmes, setFilmes] = useState([]);
+  useEffect(() => {
+    async function loadaFilmes() {
+      const { data } = await await api.get("r-api/?api=filmes");
+      console.log(data);
+      setFilmes(data);
+    }
+    loadaFilmes();
+  }, []);
   return (
-    <div className="App">
-      <h1>Page Home </h1>
+    <div className="container">
+      <main className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.nome}</strong>
+              <img src={filme.foto} alt="filme.nome" />
+              <Link to="/">Acessar</Link>
+            </article>
+          );
+        })}
+      </main>
     </div>
   );
 }
