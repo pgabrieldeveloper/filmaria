b5f80a3e731234992df462f8831e62d01e64bc9d
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-12 19:15:17 -0300

    function handleDelete

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -8,6 +8,13 @@ function Favoritos(params) {
     const myList = localStorage.getItem("filmes");
     setFilmes(JSON.parse(myList) || []);
   }, []);
+
+  function handleDelete(id) {
+    let filterMovies = filmes.filter((item) => item.id !== id);
+    setFilmes(filterMovies);
+    localStorage.setItem("filmes", JSON.stringify(filterMovies));
+  }
+
   return (
     <div className="meus-filmes">
       <h1>Meus Filmes</h1>
@@ -18,7 +25,7 @@ function Favoritos(params) {
               <span>{item.nome}</span>
               <div>
                 <Link to={`/filme/${item.id}`}>Ver detalhes </Link>
-                <button>Excluir</button>
+                <button onClick={() => handleDelete(item.id)}>Excluir</button>
               </div>
             </li>
           );
