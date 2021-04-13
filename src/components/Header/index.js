22b333ee8c298cb085d568847b7fc5b8a7bdfbf6
pgabrieldeveloper <g4brieldev@gmail.com>
2021-04-12 22:02:00 -0300

    Add links for contact

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,14 +2,27 @@ import "./style.css";
 import { Link } from "react-router-dom";
 function Header() {
   return (
-    <header>
-      <Link className="logo" to="/">
-        Filmaria
-      </Link>
-      <Link className="favoritos" to="/favoritos">
-        Salvos
-      </Link>
-    </header>
+    <>
+      <header>
+        <Link className="logo" to="/">
+          Filmaria
+        </Link>
+        <Link className="favoritos" to="/favoritos">
+          Salvos
+        </Link>
+      </header>
+      <div className="contato">
+        <a target="blank" href="https://github.com/pgabrieldeveloper">
+          Git Hub
+        </a>
+        <a
+          target="blank"
+          href="https://www.linkedin.com/in/paulo-gabriel-gomes-matias-969b041b8/"
+        >
+          Linkedin
+        </a>
+      </div>
+    </>
   );
 }
 
